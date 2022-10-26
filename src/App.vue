58674d5e13044d2173a9e4aeab9e6013c7ce5934
Amanda Alves <manda.alvs@gmail.com>
2022-10-26 11:15:04 -0300

    using transition events

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="container">
     <div class="block" :class="{animate: animatedBlock}"></div>
-    <button @click="animatedBlock">Animate</button>
-  </div> 
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+    <button @click="animateBlock">Animate</button>
+  </div>
+  <div class="container">
+    <transition name="para" @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter"
+      @before-leave="beforeLeave" @leave="leave" @after-leave="afterLeave">
+      <p v-if="paraIsVisible">This is only sometimes visible...</p>
+    </transition>
+    <button @click="toggleParagraph">Toggle Paragraph</button>
+  </div>
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
+  </div>
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
@@ -17,17 +30,53 @@ export default {
   data() {
     return {
       animatedBlock: false,
-      dialogIsVisible: false };
+      dialogIsVisible: false,
+      paraIsVisible: false,
+      usersAreVisible: false,
+    };
   },
   methods: {
+    beforeEnter(el) {
+      console.log('beforeEnter');
+      console.log(el)
+    },
+    enter(el){
+      console.log(el);
+      console.log('enter');
+    },  
+    afterEnter(el){
+      console.log('afterEnter');
+      console.log(el);
+    },
+    beforeLeave(el){
+      console.log('BeforeLeave');
+      console.log(el);
+    },
+    leave(el){
+      console.log('leave');
+      console.log(el);
+    },
+    afterLeave(el) {
+      console.log('afterLeave');
+      console.log(el);
+    },
+    showUsers() {
+      this.usersAreVisible = true;
+    },
+    hideUsers() {
+      this.usersAreVisible = false;
+    },
     animateBlock() {
-      
+      this.animatedBlock = true;
     },
     showDialog() {
       this.dialogIsVisible = true;
     },
     hideDialog() {
       this.dialogIsVisible = false;
+    },
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible;
     },
   },
 };
@@ -40,12 +89,15 @@ export default {
 * {
   box-sizing: border-box;
 }
+
 html {
   font-family: sans-serif;
 }
+
 body {
   margin: 0;
 }
+
 button {
   font: inherit;
   padding: 0.5rem 2rem;
@@ -55,18 +107,21 @@ button {
   color: white;
   cursor: pointer;
 }
+
 button:hover,
 button:active {
   background-color: #a80b48;
   border-color: #a80b48;
 }
+
 .block {
   width: 8rem;
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
-  
+
 }
+
 .container {
   max-width: 40rem;
   margin: 2rem auto;
@@ -79,9 +134,31 @@ button:active {
   border-radius: 12px;
 }
 
-.animate { 
+.animate {
 
   animation: slide-fade 0.3s ease-out forwards;
+}
+
+.para-leave-active {
+  animation: slide-scale 0.3s ease-out;
+}
+
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
 }
 
 @keyframes slide-fade {
